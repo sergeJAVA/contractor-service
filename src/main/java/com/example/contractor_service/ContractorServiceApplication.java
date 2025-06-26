d919e2fb871c39b3a76cd22503fc4703755635c9
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-06-26 20:57:22 +0300

    chore: add checkstyle plugin and XML files.

--- src/main/java/com/example/contractor_service/ContractorServiceApplication.java
+++ src/main/java/com/example/contractor_service/ContractorServiceApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContractorServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContractorServiceApplication.class, args);
-	}
+public static void main(String[] args) {
+
+SpringApplication.run(ContractorServiceApplication.class, args);
+
+}
 
 }
