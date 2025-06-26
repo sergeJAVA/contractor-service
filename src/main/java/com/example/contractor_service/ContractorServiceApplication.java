10a31af8c83ddac5d049e64b71688028b887a246
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-06-26 20:28:39 +0300

    first commit

---
+++ src/main/java/com/example/contractor_service/ContractorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.contractor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractorServiceApplication.class, args);
+	}
+
+}
